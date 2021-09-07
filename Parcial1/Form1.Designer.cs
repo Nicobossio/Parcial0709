fb03ebe1220980cd1bda42a3e41341af790f741c
nicob <nicob@DESKTOP-6635PRH>
2021-09-06 21:41:19 -0500

    Agregar archivos de proyecto.

---
+++ Parcial1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Parcial1
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.rtxtContainer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(38, 102);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(218, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.rtxtContainer_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(38, 175);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(218, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkCyan;
+            this.lblTitle.Location = new System.Drawing.Point(35, 33);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(144, 13);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "BIENVENIDO A ONLYPANS";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(38, 234);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.TabIndex = 4;
+            this.btnJoin.Text = "Ingresar";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(35, 77);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(43, 13);
+            this.lblUser.TabIndex = 5;
+            this.lblUser.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contraseña";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(163, 234);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(78, 23);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "Registrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // rtxtContainer
+            // 
+            this.rtxtContainer.Location = new System.Drawing.Point(347, 26);
+            this.rtxtContainer.Name = "rtxtContainer";
+            this.rtxtContainer.Size = new System.Drawing.Size(201, 231);
+            this.rtxtContainer.TabIndex = 8;
+            this.rtxtContainer.Text = "";
+            this.rtxtContainer.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 321);
+            this.Controls.Add(this.rtxtContainer);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtName);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.RichTextBox rtxtContainer;
+    }
+}
+
---
+++ Parcial1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1
+{
+    public partial class Home : Form
+    {
+      
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            string Nombre = txtName.Text;
+            string password = txtPassword.Text;
+            
+            
+           
+            if (this.txtName.Text == "Nicobossio" && this.txtPassword.Text == "Nikoebr42")
+            {
+                Form2 llamar = new Form2();
+                llamar.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta, revise si estan bien escritos");
+            }
+
+            txtName.Clear();
+            txtPassword.Clear();
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void rtxtContainer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    
+
