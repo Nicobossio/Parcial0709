3382e168a2ddef8f0e47b6150176e1fec46ef0bd
Nicobossio42 <nicobossior42@gmail.com>
2021-09-06 22:51:10 -0500

    Cambios en diseño form 2 y añadidura de un tercer formulario

--- Parcial1/Form2.Designer.cs
+++ Parcial1/Form2.Designer.cs
@@ -29,12 +29,45 @@ namespace Parcial1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblSold = new System.Windows.Forms.Label();
+            this.rtxtVentas = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblSold
+            // 
+            this.lblSold.AutoSize = true;
+            this.lblSold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.35F);
+            this.lblSold.Location = new System.Drawing.Point(52, 23);
+            this.lblSold.Name = "lblSold";
+            this.lblSold.Size = new System.Drawing.Size(44, 15);
+            this.lblSold.TabIndex = 0;
+            this.lblSold.Text = "Ventas";
+            // 
+            // rtxtVentas
+            // 
+            this.rtxtVentas.Location = new System.Drawing.Point(55, 54);
+            this.rtxtVentas.Name = "rtxtVentas";
+            this.rtxtVentas.Size = new System.Drawing.Size(690, 273);
+            this.rtxtVentas.TabIndex = 1;
+            this.rtxtVentas.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtxtVentas);
+            this.Controls.Add(this.lblSold);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSold;
+        private System.Windows.Forms.RichTextBox rtxtVentas;
     }
 }
---
+++ Parcial1/Form3.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Parcial1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblType = new System.Windows.Forms.Label();
+            this.cboxAliñado = new System.Windows.Forms.CheckBox();
+            this.cboxNoaliñado = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(67, 36);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(106, 13);
+            this.lblType.TabIndex = 0;
+            this.lblType.Text = "Escoja el tipo de pan";
+            // 
+            // cboxAliñado
+            // 
+            this.cboxAliñado.AutoSize = true;
+            this.cboxAliñado.Location = new System.Drawing.Point(75, 91);
+            this.cboxAliñado.Name = "cboxAliñado";
+            this.cboxAliñado.Size = new System.Drawing.Size(83, 17);
+            this.cboxAliñado.TabIndex = 1;
+            this.cboxAliñado.Text = "Pan Aliñado";
+            this.cboxAliñado.UseVisualStyleBackColor = true;
+            // 
+            // cboxNoaliñado
+            // 
+            this.cboxNoaliñado.AutoSize = true;
+            this.cboxNoaliñado.Location = new System.Drawing.Point(75, 141);
+            this.cboxNoaliñado.Name = "cboxNoaliñado";
+            this.cboxNoaliñado.Size = new System.Drawing.Size(98, 17);
+            this.cboxNoaliñado.TabIndex = 2;
+            this.cboxNoaliñado.Text = "Pan no Aliñado";
+            this.cboxNoaliñado.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(75, 190);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(87, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Pan especial";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(207, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(297, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(297, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre del comprador";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cedula";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Cantidad de panes";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.cboxNoaliñado);
+            this.Controls.Add(this.cboxAliñado);
+            this.Controls.Add(this.lblType);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.CheckBox cboxAliñado;
+        private System.Windows.Forms.CheckBox cboxNoaliñado;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
